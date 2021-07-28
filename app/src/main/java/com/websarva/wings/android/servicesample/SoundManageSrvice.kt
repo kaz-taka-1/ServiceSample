146453d479378606bc69bbd76b22d1bb15bd8bde
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-28 12:41:15 +0900

    通知チャンネルの作成

--- app/src/main/java/com/websarva/wings/android/servicesample/SoundManageSrvice.kt
+++ app/src/main/java/com/websarva/wings/android/servicesample/SoundManageSrvice.kt
@@ -1,13 +1,22 @@
 package com.websarva.wings.android.servicesample
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.IBinder
 import android.view.View
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class SoundManageService : Service() {
+    companion object{
+        private const val CHANNEL_ID = "soundmanagerservice_notification_channel"
+    }
+
+
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
@@ -15,6 +24,11 @@ class SoundManageService : Service() {
     private var _player: MediaPlayer? = null
     override fun onCreate(){
         _player = MediaPlayer()
+        val name = getString(R.string.notification_channel_name)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID,name,importance)
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -44,6 +58,13 @@ class SoundManageService : Service() {
     }
     private inner class PlayerCompletionListener :MediaPlayer.OnCompletionListener{
         override fun onCompletion(mp: MediaPlayer?) {
+            val builder = NotificationCompat.Builder(this@SoundManageService,CHANNEL_ID)
+            builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+            builder.setContentTitle(getString(R.string.msg_notification_title_finish))
+            builder.setContentText(getString(R.string.msg_notification_text_finish))
+            val notification = builder.build()
+            val manager = NotificationManagerCompat.from(this@SoundManageService)
+            manager.notify(100,notification)
             stopSelf()
         }
     }
